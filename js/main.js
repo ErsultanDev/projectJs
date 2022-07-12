74e28c96174a995cde23efb6aac4166dd44a4d28
Ersultan <example@gmail.com>
2022-07-12 12:38:38 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+  for (let j = 0; j < 6; j++) {
+    console.log(j);
+  }
+}
+
+let result = "";
+const length = 7;
+
+
+
+console.log(result);
